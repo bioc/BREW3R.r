ea187655df67a5279f73f58afa11818ca65fa048
Lucille Delisle <lucille.delisle@epfl.ch>
2024-03-27 18:19:39 +0100

    uncomment tests

--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -447,238 +447,238 @@ input_gr_case9_pot_extended <- GenomicRanges::GRanges(
     old_end = c(25, 70)
 )
 
-# test_that("extract_last_exon works", {
-#     expect_equal(extract_last_exon(input_gr, "gene_id"), input_gr[2:3])
-#     expect_equal(extract_last_exon(input_gr, "transcript_id"), input_gr[1:3])
-#     expect_equal(extract_last_exon(input_gr, "gene_id", invert = TRUE),
-#                  input_gr[c(1, 4)])
-#     expect_equal(extract_last_exon(input_gr, "transcript_id", invert  = TRUE),
-#                  input_gr[4])
-# })
-#
-# test_that("get_extending_overlap works", {
-#     expect_equal(
-#         get_extending_overlap(input_gr, input_gr_to_overlap),
-#         small_gr
-#     )
-# })
-#
-# test_that("apply_coo_changes works", {
-#     expect_equal(
-#         apply_coo_changes(small_gr),
-#         small_gr_pot_extended
-#     )
-# })
-#
-# test_that("adjust_for_collision works on no issue", {
-#     expect_equal(
-#         adjust_for_collision(small_gr_pot_extended),
-#         list(
-#             pot_issues = data.frame(),
-#             new_gr = small_gr_pot_extended
-#         )
-#     )
-# })
-#
-# test_that("case1 works", {
-#     expect_equal(
-#         apply_coo_changes(
-#             get_extending_overlap(extract_last_exon(input_gr_case1),
-#                                   input_to_overlap_case1_2_3_4_6_7_8)
-#         ),
-#         subset(
-#             input_gr_case1_pot_extended,
-#             old_start != start | old_end != end
-#         )
-#     )
-#     expect_equal(
-#         adjust_for_collision(input_gr_case1_pot_extended)[["new_gr"]],
-#         input_gr_case1_extended_fixed
-#     )
-# })
-#
-# test_that("case2 works", {
-#     expect_equal(
-#         apply_coo_changes(
-#             get_extending_overlap(extract_last_exon(input_gr_case2),
-#                                   input_to_overlap_case1_2_3_4_6_7_8)
-#             ),
-#         subset(
-#             input_gr_case2_pot_extended,
-#             old_start != start | old_end != end
-#         )
-#     )
-#     expect_equal(
-#         adjust_for_collision(input_gr_case2_pot_extended)[["new_gr"]],
-#         input_gr_case2_extended_fixed
-#     )
-# })
-#
-# test_that("case3 works", {
-#     expect_equal(
-#         apply_coo_changes(
-#             get_extending_overlap(extract_last_exon(input_gr_case3_5),
-#                                   input_to_overlap_case1_2_3_4_6_7_8)
-#             ),
-#         subset(
-#             input_gr_case3_pot_extended,
-#             old_start != start | old_end != end
-#         )
-#     )
-#     expect_equal(
-#         adjust_for_collision(input_gr_case3_pot_extended)[["new_gr"]],
-#         input_gr_case3_extended_fixed
-#     )
-# })
-#
-# test_that("case4 works", {
-#     expect_equal(
-#         apply_coo_changes(
-#             get_extending_overlap(extract_last_exon(input_gr_case4),
-#                                   input_to_overlap_case1_2_3_4_6_7_8)
-#         ),
-#         subset(
-#             input_gr_case4_pot_extended,
-#             old_start != start | old_end != end
-#         )
-#     )
-#     expect_equal(
-#         adjust_for_collision(input_gr_case4_pot_extended)[["new_gr"]],
-#         input_gr_case4_extended_fixed
-#     )
-# })
-#
-#
-# test_that("case4bis works", {
-#     expect_equal(
-#         apply_coo_changes(
-#             get_extending_overlap(extract_last_exon(input_gr_case4bis),
-#                                   input_to_overlap_case1_2_3_4_6_7_8)
-#         ),
-#         subset(
-#             input_gr_case4bis_pot_extended,
-#             old_start != start | old_end != end
-#         )
-#     )
-#     expect_equal(
-#         adjust_for_collision(input_gr_case4bis_pot_extended)[["new_gr"]],
-#         input_gr_case4bis_extended_fixed
-#     )
-# })
-#
-# test_that("case5 works", {
-#     expect_equal(
-#         apply_coo_changes(
-#             get_extending_overlap(extract_last_exon(input_gr_case3_5),
-#                                   input_to_overlap_case5_9)
-#         ),
-#         subset(
-#             input_gr_case5_pot_extended,
-#             old_start != start | old_end != end
-#         )
-#     )
-#     expect_equal(
-#         adjust_for_collision(input_gr_case5_pot_extended)[["new_gr"]],
-#         input_gr_case5_extended_fixed
-#     )
-# })
-#
-# test_that("case6 works", {
-#     expect_equal(
-#         apply_coo_changes(
-#             get_extending_overlap(extract_last_exon(input_gr_case6),
-#                                   input_to_overlap_case1_2_3_4_6_7_8)
-#         ),
-#         subset(
-#             input_gr_case6_pot_extended,
-#             old_start != start | old_end != end
-#         )
-#     )
-#     expect_equal(
-#         adjust_for_collision(input_gr_case6_pot_extended)[["new_gr"]],
-#         input_gr_case6_pot_extended
-#     )
-# })
-#
-# test_that("case6bis works", {
-#     expect_equal(
-#         apply_coo_changes(
-#             get_extending_overlap(extract_last_exon(input_gr_case6bis),
-#                                   input_to_overlap_case1_2_3_4_6_7_8)
-#         ),
-#         subset(
-#             input_gr_case6bis_pot_extended,
-#             old_start != start | old_end != end
-#         )
-#     )
-#     expect_equal(
-#         adjust_for_collision(input_gr_case6bis_pot_extended)[["new_gr"]],
-#         input_gr_case6bis_pot_extended
-#     )
-#     expect_equal(
-#         sort(adjust_for_collision(
-#             input_gr_case6bis_pot_extended[c(2,1,3)]
-#             )[["new_gr"]]),
-#         sort(input_gr_case6bis_pot_extended)
-#     )
-# })
-#
-# test_that("case7 works", {
-#     expect_equal(
-#         apply_coo_changes(
-#             get_extending_overlap(extract_last_exon(input_gr_case7),
-#                                   input_to_overlap_case1_2_3_4_6_7_8)
-#         ),
-#         subset(
-#             input_gr_case7_pot_extended,
-#             old_start != start | old_end != end
-#         )
-#     )
-#     expect_equal(
-#         adjust_for_collision(input_gr_case7_pot_extended)[["new_gr"]],
-#         input_gr_case7_pot_extended
-#     )
-# })
-#
-# test_that("case8 works", {
-#     expect_equal(
-#         apply_coo_changes(
-#             get_extending_overlap(extract_last_exon(input_gr_case8),
-#                                   input_to_overlap_case1_2_3_4_6_7_8)
-#         ),
-#         subset(
-#             input_gr_case8_pot_extended,
-#             old_start != start | old_end != end
-#         )
-#     )
-#     expect_equal(
-#         adjust_for_collision(input_gr_case8_pot_extended)[["new_gr"]],
-#         input_gr_case8_extended_fixed
-#     )
-# })
-#
-# test_that("add_new_exons works on case 5", {
-#     expect_equal(
-#         add_new_exons(
-#             input_gr_case5_extended_fixed,
-#             input_to_overlap_case5_9
-#             ),
-#         GenomicRanges::GRanges(
-#             seqnames = "chr1",
-#             ranges = IRanges::IRanges(start = c(5, 20, 33, 45, 72),
-#                                       end = c(19, 25, 40, 60, 75)),
-#             strand = "+",
-#             gene_id = rep(c("gene1", "gene2"), c(1, 4)),
-#             transcript_id = rep(c("transcript1", "transcript2"), c(1, 4)),
-#             type = "exon",
-#             exon_id = c("exon1", "exon2", "BREW3R0000000001",
-#                         "BREW3R0000000002", "BREW3R0000000003"),
-#             old_start = c(5, 20, NA, NA, NA),
-#             old_end = c(10, 22, NA, NA, NA),
-#             exon_number = c(1, 1:4)
-#         )
-#     )
-# })
+test_that("extract_last_exon works", {
+    expect_equal(extract_last_exon(input_gr, "gene_id"), input_gr[2:3])
+    expect_equal(extract_last_exon(input_gr, "transcript_id"), input_gr[1:3])
+    expect_equal(extract_last_exon(input_gr, "gene_id", invert = TRUE),
+                 input_gr[c(1, 4)])
+    expect_equal(extract_last_exon(input_gr, "transcript_id", invert  = TRUE),
+                 input_gr[4])
+})
+
+test_that("get_extending_overlap works", {
+    expect_equal(
+        get_extending_overlap(input_gr, input_gr_to_overlap),
+        small_gr
+    )
+})
+
+test_that("apply_coo_changes works", {
+    expect_equal(
+        apply_coo_changes(small_gr),
+        small_gr_pot_extended
+    )
+})
+
+test_that("adjust_for_collision works on no issue", {
+    expect_equal(
+        adjust_for_collision(small_gr_pot_extended),
+        list(
+            pot_issues = data.frame(),
+            new_gr = small_gr_pot_extended
+        )
+    )
+})
+
+test_that("case1 works", {
+    expect_equal(
+        apply_coo_changes(
+            get_extending_overlap(extract_last_exon(input_gr_case1),
+                                  input_to_overlap_case1_2_3_4_6_7_8)
+        ),
+        subset(
+            input_gr_case1_pot_extended,
+            old_start != start | old_end != end
+        )
+    )
+    expect_equal(
+        adjust_for_collision(input_gr_case1_pot_extended)[["new_gr"]],
+        input_gr_case1_extended_fixed
+    )
+})
+
+test_that("case2 works", {
+    expect_equal(
+        apply_coo_changes(
+            get_extending_overlap(extract_last_exon(input_gr_case2),
+                                  input_to_overlap_case1_2_3_4_6_7_8)
+            ),
+        subset(
+            input_gr_case2_pot_extended,
+            old_start != start | old_end != end
+        )
+    )
+    expect_equal(
+        adjust_for_collision(input_gr_case2_pot_extended)[["new_gr"]],
+        input_gr_case2_extended_fixed
+    )
+})
+
+test_that("case3 works", {
+    expect_equal(
+        apply_coo_changes(
+            get_extending_overlap(extract_last_exon(input_gr_case3_5),
+                                  input_to_overlap_case1_2_3_4_6_7_8)
+            ),
+        subset(
+            input_gr_case3_pot_extended,
+            old_start != start | old_end != end
+        )
+    )
+    expect_equal(
+        adjust_for_collision(input_gr_case3_pot_extended)[["new_gr"]],
+        input_gr_case3_extended_fixed
+    )
+})
+
+test_that("case4 works", {
+    expect_equal(
+        apply_coo_changes(
+            get_extending_overlap(extract_last_exon(input_gr_case4),
+                                  input_to_overlap_case1_2_3_4_6_7_8)
+        ),
+        subset(
+            input_gr_case4_pot_extended,
+            old_start != start | old_end != end
+        )
+    )
+    expect_equal(
+        adjust_for_collision(input_gr_case4_pot_extended)[["new_gr"]],
+        input_gr_case4_extended_fixed
+    )
+})
+
+
+test_that("case4bis works", {
+    expect_equal(
+        apply_coo_changes(
+            get_extending_overlap(extract_last_exon(input_gr_case4bis),
+                                  input_to_overlap_case1_2_3_4_6_7_8)
+        ),
+        subset(
+            input_gr_case4bis_pot_extended,
+            old_start != start | old_end != end
+        )
+    )
+    expect_equal(
+        adjust_for_collision(input_gr_case4bis_pot_extended)[["new_gr"]],
+        input_gr_case4bis_extended_fixed
+    )
+})
+
+test_that("case5 works", {
+    expect_equal(
+        apply_coo_changes(
+            get_extending_overlap(extract_last_exon(input_gr_case3_5),
+                                  input_to_overlap_case5_9)
+        ),
+        subset(
+            input_gr_case5_pot_extended,
+            old_start != start | old_end != end
+        )
+    )
+    expect_equal(
+        adjust_for_collision(input_gr_case5_pot_extended)[["new_gr"]],
+        input_gr_case5_extended_fixed
+    )
+})
+
+test_that("case6 works", {
+    expect_equal(
+        apply_coo_changes(
+            get_extending_overlap(extract_last_exon(input_gr_case6),
+                                  input_to_overlap_case1_2_3_4_6_7_8)
+        ),
+        subset(
+            input_gr_case6_pot_extended,
+            old_start != start | old_end != end
+        )
+    )
+    expect_equal(
+        adjust_for_collision(input_gr_case6_pot_extended)[["new_gr"]],
+        input_gr_case6_pot_extended
+    )
+})
+
+test_that("case6bis works", {
+    expect_equal(
+        apply_coo_changes(
+            get_extending_overlap(extract_last_exon(input_gr_case6bis),
+                                  input_to_overlap_case1_2_3_4_6_7_8)
+        ),
+        subset(
+            input_gr_case6bis_pot_extended,
+            old_start != start | old_end != end
+        )
+    )
+    expect_equal(
+        adjust_for_collision(input_gr_case6bis_pot_extended)[["new_gr"]],
+        input_gr_case6bis_pot_extended
+    )
+    expect_equal(
+        sort(adjust_for_collision(
+            input_gr_case6bis_pot_extended[c(2,1,3)]
+            )[["new_gr"]]),
+        sort(input_gr_case6bis_pot_extended)
+    )
+})
+
+test_that("case7 works", {
+    expect_equal(
+        apply_coo_changes(
+            get_extending_overlap(extract_last_exon(input_gr_case7),
+                                  input_to_overlap_case1_2_3_4_6_7_8)
+        ),
+        subset(
+            input_gr_case7_pot_extended,
+            old_start != start | old_end != end
+        )
+    )
+    expect_equal(
+        adjust_for_collision(input_gr_case7_pot_extended)[["new_gr"]],
+        input_gr_case7_pot_extended
+    )
+})
+
+test_that("case8 works", {
+    expect_equal(
+        apply_coo_changes(
+            get_extending_overlap(extract_last_exon(input_gr_case8),
+                                  input_to_overlap_case1_2_3_4_6_7_8)
+        ),
+        subset(
+            input_gr_case8_pot_extended,
+            old_start != start | old_end != end
+        )
+    )
+    expect_equal(
+        adjust_for_collision(input_gr_case8_pot_extended)[["new_gr"]],
+        input_gr_case8_extended_fixed
+    )
+})
+
+test_that("add_new_exons works on case 5", {
+    expect_equal(
+        add_new_exons(
+            input_gr_case5_extended_fixed,
+            input_to_overlap_case5_9
+            ),
+        GenomicRanges::GRanges(
+            seqnames = "chr1",
+            ranges = IRanges::IRanges(start = c(5, 20, 33, 45, 72),
+                                      end = c(19, 25, 40, 60, 75)),
+            strand = "+",
+            gene_id = rep(c("gene1", "gene2"), c(1, 4)),
+            transcript_id = rep(c("transcript1", "transcript2"), c(1, 4)),
+            type = "exon",
+            exon_id = c("exon1", "exon2", "BREW3R0000000001",
+                        "BREW3R0000000002", "BREW3R0000000003"),
+            old_start = c(5, 20, NA, NA, NA),
+            old_end = c(10, 22, NA, NA, NA),
+            exon_number = c(1, 1:4)
+        )
+    )
+})
 
 test_that("add_new_exons works on case 9", {
     expect_equal(
@@ -703,44 +703,44 @@ test_that("add_new_exons works on case 9", {
     )
 })
 
-#
-# test_that("add_new_exons works when run twice", {
-#     expect_equal(
-#         sort(add_new_exons(
-#             GenomicRanges::GRanges(
-#                 seqnames = "chr1",
-#                 ranges = IRanges::IRanges(start = c(5, 33, 45, 55),
-#                                           end = c(25, 40, 54, 70)),
-#                 strand = "+",
-#                 gene_id = rep(c("gene1", "gene2"), c(3, 1)),
-#                 transcript_id = rep(c("transcript1", "transcript2"), c(3, 1)),
-#                 type = "exon",
-#                 exon_id = c("exon1", "BREW3R0000000001", "BREW3R0000000002",
-#                             "exon2"),
-#                 exon_number = c(1:3, 1)
-#             ),
-#             GenomicRanges::GRanges(
-#                 seqnames = "chr1",
-#                 ranges = IRanges::IRanges(start = c(60, 92),
-#                                           end = c(70, 95)),
-#                 strand = "+",
-#                 gene_id = "geneA",
-#                 transcript_id = "transcriptA",
-#                 type = "exon",
-#                 exon_id = c("exonA", "exonB")
-#             )
-#         )),
-#         sort(GenomicRanges::GRanges(
-#             seqnames = "chr1",
-#             ranges = IRanges::IRanges(start = c(5, 33, 45, 55, 92),
-#                                       end = c(25, 40, 54, 70, 95)),
-#             strand = "+",
-#             gene_id = rep(c("gene1", "gene2"), c(3, 2)),
-#             transcript_id = rep(c("transcript1", "transcript2"), c(3, 2)),
-#             type = "exon",
-#             exon_id = c("exon1", "BREW3R0000000001", "BREW3R0000000002",
-#                         "exon2", "BREW3R0000000003"),
-#             exon_number = c(1:3, 1:2)
-#         ))
-#     )
-# })
+
+test_that("add_new_exons works when run twice", {
+    expect_equal(
+        sort(add_new_exons(
+            GenomicRanges::GRanges(
+                seqnames = "chr1",
+                ranges = IRanges::IRanges(start = c(5, 33, 45, 55),
+                                          end = c(25, 40, 54, 70)),
+                strand = "+",
+                gene_id = rep(c("gene1", "gene2"), c(3, 1)),
+                transcript_id = rep(c("transcript1", "transcript2"), c(3, 1)),
+                type = "exon",
+                exon_id = c("exon1", "BREW3R0000000001", "BREW3R0000000002",
+                            "exon2"),
+                exon_number = c(1:3, 1)
+            ),
+            GenomicRanges::GRanges(
+                seqnames = "chr1",
+                ranges = IRanges::IRanges(start = c(60, 92),
+                                          end = c(70, 95)),
+                strand = "+",
+                gene_id = "geneA",
+                transcript_id = "transcriptA",
+                type = "exon",
+                exon_id = c("exonA", "exonB")
+            )
+        )),
+        sort(GenomicRanges::GRanges(
+            seqnames = "chr1",
+            ranges = IRanges::IRanges(start = c(5, 33, 45, 55, 92),
+                                      end = c(25, 40, 54, 70, 95)),
+            strand = "+",
+            gene_id = rep(c("gene1", "gene2"), c(3, 2)),
+            transcript_id = rep(c("transcript1", "transcript2"), c(3, 2)),
+            type = "exon",
+            exon_id = c("exon1", "BREW3R0000000001", "BREW3R0000000002",
+                        "exon2", "BREW3R0000000003"),
+            exon_number = c(1:3, 1:2)
+        ))
+    )
+})
